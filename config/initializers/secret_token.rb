eabb926206c9294ad7687fe879beee100053b32e
Virtual Machine Administrator <administrator@ubuntu-ror-dev-env.(none)>
2012-01-02 13:39:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbic::Application.config.secret_token = '78bb4cdcf0cf47f0a9e6fb12dc7f1ac48f1cae1726160b21cfa185ad94d261042ced47e9a68c48752f052ecef4dc6fabfdfe7a7629f05ecb620d428955c295ac'
