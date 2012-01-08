9cfd6c7aaa98b5aa0c7f4927caf81f1937db13f8
Virtual Machine Administrator <administrator@ubuntu-ror-dev-env.(none)>
2012-01-08 19:52:08 +0000

    Finished adding species

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,8 +1,49 @@
 class SpeciesController < ApplicationController
 
+  
+  def index
+    @title = "All Species"
+    @species = Species.paginate(:page => params[:page])
+  end  
+
+  def show
+    @species = Species.find(params[:id])
+    @title = @species.species_name
+  end
+
   def new
     @species = Species.new
   end
 
+  def create
+    @species = Species.new(params[:species])
+    if @species.save
+    flash[:success] = "Species record saved!"
+      redirect_to @species
+    else
+      @title = "Capture Species Details"
+      render 'new'
+    end
+  end
 
+  def edit
+    @species = Species.find(params[:id])
+    @title = "Edit species"
+  end
+
+  def update
+    if @species.update_attributes(params[:species])
+      flash[:success] = "species updated."
+      redirect_to @species
+    else
+      @title = "Edit species"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Species.find(params[:id]).destroy
+    flash[:success] = "Sspecies Record Deleted."
+    redirect_to species_path
+  end
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,4 +1,5 @@
 class Species < ActiveRecord::Base
 attr_accessible :species_name, :state, :abundance, :location, :county, :grid_reference, :provenance, :start_date, :end_date, :observer, :determiner, :record_type, :reference, :comment
+validates_presence_of :species_name, :location, :county, :grid_reference, :start_date, :observer
 
 end
